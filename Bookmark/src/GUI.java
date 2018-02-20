307e7d7975e66577a0c1e29726d9039ce01a9be3
Amzad Chowdhury <skytearz@live.com>
2018-02-20 11:11:49 -0500

    Creation

--- Bookmark/src/GUI.java
+++ Bookmark/src/GUI.java
@@ -3,5 +3,9 @@ import javax.swing.JFrame;
 public class GUI {
 	JFrame jFrame = new JFrame("Bookmark");
 	
+	public GUI() {
+		jFrame.setSize(700, 500);
+		// jFrame.setLayout();
+	}
 
 }
--- Bookmark/src/Initialize.java
+++ Bookmark/src/Initialize.java
@@ -2,7 +2,13 @@
 public class Initialize {
 	
 	public static void main(String[] args) {
-		
+		if (args.length > 0) {
+			//figure out the input flags and pass them
+			new BookmarkCLI();
+		}
+		else {
+			new GUI();
+		}
 	}
 
 }
