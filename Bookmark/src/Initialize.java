4b0d21c7a772b11ac523ef0f2f868cea85a3fd6c
Amzad <Amzad@amzadchowdhury.com>
2018-02-25 22:56:21 -0500

    Updated

---
+++ Bookmark/src/Initialize.java
@@ -0,0 +1,8 @@
+
+public class Initialize {
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+		
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CS-370
+Software Engineering - Semester long project
